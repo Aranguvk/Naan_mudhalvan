93dfec073aabe34375d3792b1a0c8f442a0a5cff
Aranguvk <145587156+Aranguvk@users.noreply.github.com>
2023-10-03 14:36:51 +0530

    Create CHALLENGE 1.2.py

---
+++ CHALLENGE 1.2.py
@@ -0,0 +1,9 @@
+num = int(input("Enter the Number :"))
+def factorial(n):
+      if (n==1 or n==0):
+           return 1
+      else:
+           return (n * factorial(n - 1))
+
+print("number : ",num)
+print("Factorial : ",factorial(num))
